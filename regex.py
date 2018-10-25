d72fa853ac736bcb95bf737363adb212338df47a
Cintronny <42981001+Cintronny@users.noreply.github.com>
2018-10-25 15:44:17 -0400

    Added Comments

--- regex.py
+++ regex.py
@@ -6,7 +6,7 @@ def finder(word):
 
     if type(word) != str:
         raise TypeError("this is not a string")
-
+#this catches if the argument is a string and raises a TypeError if it is not
     matches = re.findall(r"\w*at\b", sendo)
 
     findings = []
@@ -14,6 +14,7 @@ def finder(word):
     for word in matches:
 
         if len(word) > 3:
+            #this searches through the list and removes the words that are 3 letters or less
             findings.append(word)
     print findings
 finder(sendo) 
